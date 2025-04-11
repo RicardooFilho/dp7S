afc97ad950d81177b043f01f2d9d59959bbb196d
Ricardo Filho <ricardo-tagami@hotmail.com>
2025-04-11 19:47:55 -0300

    inicio da prova

---
+++ provaBonam/src/main/java/com/provaBonam/ProvaBonamApplication.java
@@ -0,0 +1,13 @@
+package com.provaBonam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaBonamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaBonamApplication.class, args);
+	}
+
+}
---
+++ provaBonam/src/test/java/com/provaBonam/ProvaBonamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provaBonam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaBonamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
